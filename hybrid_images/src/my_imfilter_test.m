5cb275b7fc73de0d1004f008f7563e8d8eb75390
Charis Mitsakis <cmitsakis.git@protonmail.com>
2021-04-18 01:52:11 +0300

    add tests for my_imfilter

---
+++ hybrid_images/src/my_imfilter_test.m
@@ -0,0 +1,71 @@
+% this script has test cases to help you test my_imfilter() which you will
+% write. You should verify that you get reasonable output here before using
+% your filtering to construct a hybrid image in proj1.m. The outputs are
+% all saved and you can include them in your writeup. You can add calls to
+% imfilter() if you want to check that my_imfilter() is doing something
+% similar.
+close all
+
+%% Setup
+%test_image = im2single(imread('../data/cameraman.tif'));
+test_image = im2single(imread('../data/pair2_joker.png'));
+test_image = imresize(test_image, 0.7, 'bilinear'); %resizing to speed up testing
+
+%% Identify filter
+%This filter should do nothing regardless of the padding method you use.
+identity_filter = [0 0 0; 0 1 0; 0 0 0];
+
+identity_image  = im2single(my_imfilter(test_image, identity_filter));
+identity_image0  = imfilter(test_image, identity_filter, 'conv');
+immse(identity_image, identity_image0)
+
+%% Square filter
+K = [1.5 5.7 2.1 1.7 0.5; 0.2 3.7 5.9 6.2 2.3; 0.1 8.2 5.5 4.1 2.3; 0.2 1.4 1.1 3.2 0.1; 0.3 0.2 0.1 10.2 5.7];
+K = K / sum(sum(K));
+
+filtered = im2single(my_imfilter(test_image, K));
+filtered0 = imfilter(test_image, K, 'conv', 'symmetric');
+immse(filtered, filtered0)
+
+%% Small blur with a box filter
+%This filter should remove some high frequencies
+blur_filter = [1 1 1; 1 1 1; 1 1 1];
+blur_filter = blur_filter / sum(sum(blur_filter)); %making the filter sum to 1
+
+blur_image = im2single(my_imfilter(test_image, blur_filter));
+blur_image0 = imfilter(test_image, blur_filter, 'conv', 'symmetric');
+immse(blur_image, blur_image0)
+
+% %% Large blur
+% %This blur would be slow to do directly, so we instead use the fact that
+% %Gaussian blurs are separable and blur sequentially in each direction.
+% large_1d_blur_filter = fspecial('Gaussian', [25 1], 10);
+% 
+% large_blur_image = my_imfilter(test_image, large_1d_blur_filter);
+% large_blur_image = my_imfilter(large_blur_image, large_1d_blur_filter'); %notice the ' operator which transposes the filter
+
+% %If you want to see how slow this would be to do naively, try out this
+% %equivalent operation:
+% tic %tic and toc run a timer and then print the elapsted time
+% large_blur_filter = fspecial('Gaussian', [25 25], 10);
+% large_blur_image = my_imfilter(test_image, large_blur_filter);
+% toc 
+
+%% Oriented filter (Sobel Operator)
+sobel_filter = [-1 0 1; -2 0 2; -1 0 1]; %should respond to horizontal gradients
+
+sobel_image = im2single(my_imfilter(test_image, sobel_filter));
+sobel_image0 = imfilter(test_image, sobel_filter, 'conv', 'symmetric');
+immse(sobel_image, sobel_image0)
+
+%% High pass filter (Discrete Laplacian)
+laplacian_filter = [0 1 0; 1 -4 1; 0 1 0];
+
+laplacian_image = im2single(my_imfilter(test_image, laplacian_filter));
+laplacian_image0 = imfilter(test_image, laplacian_filter, 'conv', 'symmetric');
+immse(laplacian_image, laplacian_image0)
+
+%% High pass "filter" alternative
+% high_pass_image = test_image - blur_image; %simply subtract the low frequency content
+
+%by James Hays
