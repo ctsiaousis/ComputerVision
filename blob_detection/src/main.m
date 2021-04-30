82ff9160f087c4d512f1eb0aa6d495f9ae0cec2d
Charis Mitsakis <cmitsakis.git@protonmail.com>
2021-05-01 01:06:12 +0300

    implement extrema detection

--- blob_detection/src/main.m
+++ blob_detection/src/main.m
@@ -29,3 +29,27 @@ for o = 1:num_of_octaves
     % Lowe: "Once a complete octave has been processed, we resample the Gaussian image that has twice the initial value of \sigma by taking every second pixel in each row and column"
     I2 = reduce(I2, 2*sigma);
 end
+
+% Local Extrema Detection
+extrema = cell(num_of_octaves, 1);
+for o = 1:num_of_octaves
+    extrema{o} = zeros(size(scale_space{o, i}, 1), size(scale_space{o, i}, 2), s);
+    for i = 2:log_scales_per_octave-1
+        for x = 2:size(scale_space{o, i}, 1)-2
+            for y = 2:size(scale_space{o, i}, 2)-2
+                neighbors = scale_space{o, i}(x-1:x+1, y-1:y+1)(:);
+                neighbors(5) = [];
+                % neighbors now contains 8 neighbors from the current layer
+                % we have to add 9 neighbors from the previous layer, and 9 neighbors from the next layer
+                neighbors = vertcat(neighbors, scale_space{o, i-1}(x-1:x+1, y-1:y+1)(:), scale_space{o, i+1}(x-1:x+1, y-1:y+1)(:));
+                % we now have 26 neighbors
+                sample_point = scale_space{o, i}(x, y);
+                % compare each sample point with its 26 neighbors
+                % select it if it's larger than all of these neighbors or smaller than all of them
+                if sample_point > max(neighbors) || sample_point < min(neighbors)
+                    extrema{o}(x, y, i) = 1;
+                end
+            end
+        end
+    end
+end
