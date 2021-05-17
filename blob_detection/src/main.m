4987ac9e9af53b4d719c98f0bcce96c278e6168a
ctsiaousis <ctsiaous@gmail.com>
2021-05-18 00:07:01 +0300

    need to ignore boarders from scale space

--- blob_detection/src/main.m
+++ blob_detection/src/main.m
@@ -11,7 +11,7 @@ log_scales_per_octave = s+2 % s+3 gaussian scales => s+2 LoG scales
 n = num_of_octaves * log_scales_per_octave % number of levels in scale space
 sigma = 1.6; % value recommended by Lowe
 
-threshold = 0.3 % the percentage of rejected matches
+threshold = 0.55 % the percentage of rejected matches
 
 VIS = true;
 
@@ -22,22 +22,18 @@ fprintf('Generating LoG filters with visualization: %d\n', VIS) %octave
 tic
 log_filters = generateLoGfilters(log_scales_per_octave, k, sigma, VIS);
 toc
-
 %% Generate scale-space
 fprintf('Generating scale space with visualization: %d\n', VIS) %octave
 tic
 scale_space = generateScaleSpace( I, num_of_octaves, ...
     log_scales_per_octave, log_filters, sigma, VIS);
 toc
-
-
 %% Local Extrema Detection
 fprintf('Performing Local Extrema Detection\n')
 tic
 [extrema, xPoints, yPoints, radii] = generateExtrema(num_of_octaves, ...
     log_scales_per_octave, scale_space, s, threshold);
 toc
-
 %% Display the results for each octave
 fprintf('Displaying blobs\n')
 tic
