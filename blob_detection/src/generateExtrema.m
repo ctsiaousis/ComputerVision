6b0a70f6b5fbd094effcae8a1535926b4db993d9
ctsiaousis <ctsiaous@gmail.com>
2021-05-19 21:40:13 +0300

    added Lowe's threshold

--- blob_detection/src/generateExtrema.m
+++ blob_detection/src/generateExtrema.m
@@ -7,9 +7,6 @@ function [extrema, rowVector, colVector, radiusVector] = generateExtrema(num_of_
   for o = 1:num_of_octaves
       extrema{o} = zeros(size(scale_space{o, sc}, 1), size(scale_space{o, sc}, 2), s);
       for sc = 2:log_scales_per_octave-1
-          % calculate the threshold of the current scale
-          scMaxThresh = threshold * max(max(scale_space{o, sc}));
-          scMinThresh = threshold * min(min(scale_space{o, sc}));
           for x = 2:size(scale_space{o, sc}, 1)-1
               for y = 2:size(scale_space{o, sc}, 2)-1
                   tmpMid  = scale_space{o, sc}(x-1:x+1, y-1:y+1);
@@ -22,9 +19,6 @@ function [extrema, rowVector, colVector, radiusVector] = generateExtrema(num_of_
                   if sample_point > min(neighbors) && sample_point < max(neighbors)
                       continue
                   end
-                  if sample_point >= scMinThresh && sample_point <= scMaxThresh
-                      continue
-                  end
                   % we have to add 9 neighbors from the previous layer,
                   % and 9 neighbors from the next layer
                   tmpPrev  = scale_space{o, sc-1}(x-1:x+1, y-1:y+1);
@@ -35,16 +29,19 @@ function [extrema, rowVector, colVector, radiusVector] = generateExtrema(num_of_
                   % select it if it's larger than all of these neighbors
                   % or smaller than all of them
                   if sample_point >= max(neighbors) || sample_point <= min(neighbors)
-                      % Lowe: 4.1. Eliminating Edge Responses
-                      Dxx = scale_space{o, sc}(x+1, y) - 2*scale_space{o, sc}(x, y) + scale_space{o, sc}(x-1, y);
-                      Dyy = scale_space{o, sc}(x, y+1) - 2*scale_space{o, sc}(x, y) + scale_space{o, sc}(x, y-1);
+                      % Lowe: 3.3: Eliminating Edge Responses
+                      Dxx = scale_space{o, sc}(x+1, y) + scale_space{o, sc}(x-1, y) - 2*scale_space{o, sc}(x, y);
+                      Dyy = scale_space{o, sc}(x, y+1) + scale_space{o, sc}(x, y-1) - 2*scale_space{o, sc}(x, y);
                       Dxy = scale_space{o, sc}(x-1, y+1) - scale_space{o, sc}(x-1, y-1) - scale_space{o, sc}(x+1, y+1) + scale_space{o, sc}(x+1, y-1);
                       trH = Dxx + Dyy;
                       detH = Dxx*Dyy - Dxy^2;
                       if ((trH^2 / detH) >= hessianThreshold)
                           continue
                       end
-                      extrema{o}(x, y, sc-1) = 1;
+                      % Lowe, algorithm 8
+                      if(abs(sample_point) >= threshold)
+                          extrema{o}(x, y, sc-1) = 1;
+                      end
                   end %if (possible extrema)
               end %for y
           end %for x
@@ -54,6 +51,8 @@ function [extrema, rowVector, colVector, radiusVector] = generateExtrema(num_of_
           %so we map 1->1/2, 2->1, 3->2, 4->4, 5->8, etc...
           rowVector = [rowVector; scy .* (2^(o)/4)];
           colVector = [colVector; scx .* (2^(o)/4)];
+          %bigger radius for bigger octave and scale, this means
+          %the bigger cycle is a more persistant feature
           radiusVector = [radiusVector; sc * o * ones(size(scy))];
       end %for each scale
   end %for each octave
